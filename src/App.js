78ebe35bf4da02059c4e134cf882818d0ae000d8
Cameron <cameron.cw@outlook.com>
2024-03-09 20:06:20 +0000

    Books Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import './index.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home';
+import Books from './pages/Books';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
     <div className='App'>
       <Nav />
       <Route path="/" exact component={Home}/>
-
+      <Route path="/books" component={Books}/>
       <Footer />
     </div>
     </Router>
